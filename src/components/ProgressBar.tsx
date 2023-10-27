a9c7fe18cb006c153a2006254be8d6f907b687bf
Daniele Cortesi <dani.c93@hotmail.it>
2023-10-27 14:31:42 +0200

    Add skill bars

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,21 @@
+import "./ProgressBar.scss";
+import v from "../_variables.module.scss";
+
+type Props = {
+    percentage: number,
+    className?: string,
+    children: React.ReactNode;
+};
+
+export function ProgressBar(props: Props) {
+    return (
+        <div
+            className={`${props.className} progress-bar`}
+            style={{
+                background: `radial-gradient(closest-side, ${v.backgroundContrast} 84%, transparent 85% 100%), conic-gradient(${v.accent} ${props.percentage}%, transparent 0)`,
+            }}
+        >
+            {props.children}
+        </div>
+    );
+}
--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,5 +1,6 @@
 import "./Summary.scss";
 import { MyPicture } from "./MyPicture";
+import { ProgressBar } from "./ProgressBar";
 
 type Props = {
     className?: string,
@@ -9,9 +10,39 @@ export function Summary(props: Props) {
     return (
         <div className={`${props.className} summary`}>
             <MyPicture />
+
             <h3 className="my-name">
                 Daniele Cortesi
             </h3>
+
+            <div>
+                I'm a software developer by heart and trade.
+                I love solving the riddles of programming and while doing so
+                implementing great user experiences envisioned by great designers.
+            </div>
+
+            <div className="skills">
+                <ProgressBar percentage={94}>
+                    React {<br />} Native
+                </ProgressBar>
+
+                <ProgressBar percentage={90}>
+                    React
+                </ProgressBar>
+
+                <ProgressBar percentage={60}>
+                    HTML {<br />} CSS {<br />} SASS
+                </ProgressBar>
+
+                <ProgressBar percentage={98}>
+                    Unity
+                </ProgressBar>
+
+                <ProgressBar percentage={90}>
+                    C#
+                </ProgressBar>
+            </div>
+
         </div>
     );
 }
