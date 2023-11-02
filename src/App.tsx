3f698492fc19e17ffbefa70c824b0941dfe01d75
Daniele Cortesi <dani.c93@hotmail.it>
2023-11-02 22:44:59 +0100

    Adapt to wide screens

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import { UpdatedAt } from "./components/UpdatedAt";
 function App() {
     return (
         <>
-            <Summary className="pt-10 px-8 pb-4 sm:p-4 sm:min-w-[290px] sm:max-w-[390px]" />
-            <Experience className="p-8" />
+            <Summary className="pt-10 px-8 pb-4 sm:p-4 sm:flex-1 sm:min-w-[290px] sm:max-w-[390px]" />
+            <Experience className="p-8 sm:pt-4 sm:flex-[2]" />
             <LocaleButton className="fixed top-8 right-8" />
             <UpdatedAt className="absolute bottom-8 right-8" />
         </>
