25bb423ff40f44084230198caf0b03bcad8285ef
Daniele Cortesi <dani.c93@hotmail.it>
2023-12-09 15:26:42 +0100

    Use footer tag

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ function App() {
         <>
             <Summary className="pt-10 px-8 pb-4 sm:p-4 sm:flex-1 sm:min-w-[290px] sm:max-w-[383px]" />
             <Experience className="p-8 sm:pt-4 sm:flex-[2]" />
-            <div className="absolute bottom-8 right-8 flex items-center">
+            <footer className="absolute bottom-8 right-8 flex items-center">
                 <SourceLink />
                 <span className="mx-2"> | </span>
                 <UpdatedAt />
-            </div>
+            </footer>
         </>
     );
 }
